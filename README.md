7e582e945c6c3e1ffbe1802dccb27c425a659629
Devishi Ranjan <drconker@gmail.com>
2025-03-09 15:56:40 +0530

    Create base app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Color mixer
+
+A simple app to blend two colors via the CSS `color-mix()` function.
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+
+function App() {
+	return (
+		<>
+			<h1>color-mix(er)</h1>
+			{/* TODO */}
+		</>
+	)
+}
+
+export default App
